092ebef389d084c7f6e008427a6cce45f16ec3e9
Emile Barnabe <189215222+eheh1000@users.noreply.github.com>
2024-11-28 21:13:32 +0100

    Update piece.c, finish calculAttteignableTour

--- sources/piece.c
+++ sources/piece.c
@@ -10,34 +10,32 @@ void calculAtteignablePion(Piece* self, Case Plateau[8][8]){
     int yPiece = self->y;
 
     if (self->couleur == BLANC){                                                                              //Déplacements classique
-        if(xPiece<8 && Plateau[xPiece+1][yPiece].piece==NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->coupsAtteignables);
-        }
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->casesAtteignables);
         if(xPiece == 2){
             if(Plateau[xPiece+2][yPiece].piece==NULL){                                                        //Avance de deux s'il est sur la ligne de départ
-                insertionListeCaseAtteignables(&Plateau[xPiece+2][yPiece], self->coupsAtteignables);
+                insertionListeCaseAtteignables(&Plateau[xPiece+2][yPiece], self->casesAtteignables);
             }
         }
         if(xPiece<8 && yPiece<8 && Plateau[xPiece+1][yPiece+1].piece!=NULL){                                  //Possibilité de manger en diagonale
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece+1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece+1], self->casesAtteignables);
         }
         if(xPiece<8 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece-1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece-1], self->casesAtteignables);
         }
     }else{
         if(xPiece >1 && Plateau[xPiece-1][yPiece].piece==NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->casesAtteignables);
         }
         if(xPiece == 7){
             if(Plateau[xPiece-2][yPiece].piece==NULL){
-                insertionListeCaseAtteignables(&Plateau[xPiece-2][yPiece], self->coupsAtteignables);
+                insertionListeCaseAtteignables(&Plateau[xPiece-2][yPiece], self->casesAtteignables);
             }
         }
         if(xPiece>1 && yPiece<8 && Plateau[xPiece-1][yPiece+1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece+1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece+1], self->casesAtteignables);
         }
         if(xPiece>1 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece-1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece-1], self->casesAtteignables);
         }
     }
 }
@@ -45,7 +43,57 @@ void calculAtteignablePion(Piece* self, Case Plateau[8][8]){
 void calculAtteignableTour(Piece* self, Case Plateau[8][8]){
     int xPiece = self->x;
     int yPiece = self->y;
-    while(xPiece>=1 && xPiece<=8 && yPiece>=1 && yPiece<=8 && Plateau[xPiece][yPiece].piece!=NULL ){
 
+    //Tour vers le haut (PDV des blancs)
+
+    while(xPiece<8 && Plateau[xPiece+1][yPiece].piece==NULL){                                      
+        insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->casesAtteignables);
+        xPiece++;
     }
+    if(xPiece != 7){
+        if((Plateau[xPiece+1][yPiece].piece)->couleur != self->couleur){
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->casesAtteignables);
+        }
+    }
+    int xPiece = self->x;
+
+    //Tour vers le bas (PDV des blancs)
+
+    while(xPiece>1 && Plateau[xPiece-1][yPiece].piece==NULL){
+        insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->casesAtteignables);
+        xPiece--;
+    }
+    if(xPiece != 2){
+        if((Plateau[xPiece-1][yPiece].piece)->couleur != self->couleur){
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->casesAtteignables);
+        }
+    }
+    int xPiece = self->x;
+
+    //Tour vers la droite (PDV des blancs)
+
+    while(yPiece<8 && Plateau[xPiece][yPiece+1].piece==NULL ){
+        insertionListeCaseAtteignables(&Plateau[xPiece][yPiece+1], self->casesAtteignables);
+        yPiece++;
+    }
+    if(yPiece != 7){
+        if((Plateau[xPiece][yPiece+1].piece)->couleur != self->couleur){
+            insertionListeCaseAtteignables(&Plateau[xPiece][yPiece+1], self->casesAtteignables);
+        }
+    }
+    int yPiece = self->y;
+
+    //Tour vers la gauche (PDV des blancs)
+
+    while(yPiece>1 && Plateau[xPiece][yPiece-1].piece==NULL ){
+        insertionListeCaseAtteignables(&Plateau[xPiece][yPiece-1], self->casesAtteignables);
+        yPiece--;
+    }
+    if(yPiece != 2){
+        if((Plateau[xPiece][yPiece-1].piece)->couleur != self->couleur){
+            insertionListeCaseAtteignables(&Plateau[xPiece][yPiece-1], self->casesAtteignables);
+        }
+    }
+    int yPiece = self->y;
+
 }
